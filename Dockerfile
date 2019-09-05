0dc15ef0ca06a327a0ea999a06b8fce58752aed5
SupaDupa <emin@icore.kz>
2019-09-05 20:25:59 +0600

    refactoring dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:8-jdk-alpine
 COPY target/spring-boot-kubernetes-configmap-0.0.1-SNAPSHOT.jar example-app.jar
 RUN mkdir /configs
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "super-app.jar"]
+ENTRYPOINT ["java", "-jar", "example-app.jar"]
--- src/main/java/kz/example/sbkcm/ExampleApp.java
+++ src/main/java/kz/example/sbkcm/ExampleApp.java
@@ -2,7 +2,11 @@ package kz.example.sbkcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@PropertySource(value = "file:/configs/application.properties")
 @SpringBootApplication
 public class ExampleApp {
 
