73f76364c299cfee9e042918328c27f7a4625a78
SupaDupa <emin@icore.kz>
2019-09-09 12:36:08 +0600

    asdasd

--- src/main/java/kz/example/sbkcm/ExampleController.java
+++ src/main/java/kz/example/sbkcm/ExampleController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class ExampleController {
     @Value("${example.message}")
     private String message;
 
     @GetMapping("/message")
-    public String getMessage() {
-        return message;
+    public String getMessage() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostAddress();
     }
 }
