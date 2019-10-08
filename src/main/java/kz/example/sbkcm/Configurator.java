aeb80c73c494cdb39f2e9e8621d002b22a4ed968
SupaDupa <e.mingazov001@gmail.com>
2019-10-08 20:42:43 +0600

    asd

--- src/main/java/kz/example/sbkcm/Configurator.java
+++
@@ -1,9 +0,0 @@
-package kz.example.sbkcm;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "file:/configs/application.properties")
-public class Configurator {
-}
