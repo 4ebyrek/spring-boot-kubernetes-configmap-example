dbd9072e17f8f0ee2b60234c67356ff88d659b88
SupaDupa <e.mingazov001@gmail.com>
2019-10-08 12:28:49 +0600

    asd

--- src/main/java/kz/example/sbkcm/ExampleController.java
+++ src/main/java/kz/example/sbkcm/ExampleController.java
@@ -6,16 +6,31 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @RestController
 public class ExampleController {
     @Autowired
     Environment env;
 
+    @Autowired
+    DataSource dataSource;
+
     @GetMapping
-    public String getMessage(){
+    public String getMessage() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String selectSql = "SELECT * FROM eldarbai WHERE id = ?";
+        CallableStatement clb = connection.prepareCall(selectSql);
+        clb.setString(1, "one");
+        ResultSet rs = clb.executeQuery();
+        rs.next();
+        System.out.println("name = "+ rs.getString("name"));
         return env.getProperty("example.message");
     }
 }
