b814d7f2241e2d548e4a435e1a9e8dd250f00b04
SupaDupa <emin@icore.kz>
2019-10-19 15:15:33 +0600

    asd

--- src/main/java/kz/example/sbkcm/ExampleController.java
+++ src/main/java/kz/example/sbkcm/ExampleController.java
@@ -1,5 +1,7 @@
 package kz.example.sbkcm;
 
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -14,7 +16,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static net.logstash.logback.argument.StructuredArguments.v;
+
 @RestController
+@Slf4j
 public class ExampleController {
     @Autowired
     Environment env;
@@ -25,6 +30,9 @@ public class ExampleController {
     @Value("${example.message}")
     String message;
 
+    @Autowired
+    public Stopwatch stopwatch;
+
     @GetMapping
     public String getMessage() throws SQLException {
         Connection connection = dataSource.getConnection();
@@ -33,7 +41,10 @@ public class ExampleController {
         clb.setString(1, "one");
         ResultSet rs = clb.executeQuery();
         rs.next();
-        System.out.println("name = "+ rs.getString("name"));
-        return env.getProperty("example.message") + message;
+        log.info("EEEEEEE",
+                v("example.message", env.getProperty("example.message")),
+                v("name",rs.getString("name"))
+        );
+        return null;
     }
 }
